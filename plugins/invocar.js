34b1e85bb29b1a0f845b8eb1754913edfd84202d
MENDEZITO150 <96160874+MENDEZITO150@users.noreply.github.com>
2022-07-14 17:24:14 -0500

    INVOCAR BOT V3

--- plugins/invocar.js
+++ plugins/invocar.js
@@ -20,8 +20,8 @@ let handler = async (m, { conn, participants, args }) => {
     let pesan = args.join` `
     let oi = `*MENSAJE:* ${pesan}`
     let hmm = `*💥 I N V O C A R - G R U P O 💥*\n\n`
-    let duh = `🐱\n\n*▌│█║▌║▌║║▌║▌║▌║█*\n               🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 🐈`
-       var teks = `${oi}\n\n🐱\n`
+    let duh = `🐱‍🐉\n\n*▌│█║▌║▌║║▌║▌║▌║█*\n               🐱‍🐉 WOLF BOT 🐱‍🐉`
+       var teks = `${oi}\n\n🐱‍🐉\n`
     for (let admon of groupMembers) {
         teks += ` ┣↬ @${admon.jid.split('@')[0]}\n`
     }
